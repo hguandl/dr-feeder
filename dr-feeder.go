fb47f71cfbd1ddf88a867a9f99e9c535c1ff1111
Hao Guan <hguandl@gmail.com>
2021-05-23 13:39:14 +0800

    Bump version to 2.3.4

--- dr-feeder.go
+++ dr-feeder.go
@@ -14,7 +14,7 @@ import (
 )
 
 // Version is current `git describe --tags` infomation.
-var Version string = "v2.3.3"
+var Version string = "v2.3.4"
 
 func consume(ch chan common.NotifyPayload, notifiers []notifier.Notifier) {
 	for {
